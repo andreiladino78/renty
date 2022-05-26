7fe05953c61fa314286ae97f8ed2c8d5ac4ccd32
andrei <aladino@bch.bancodechile.cl>
2022-05-25 19:14:31 -0500

    cambiar url servicio

--- src/services/create.user.service.js
+++ src/services/create.user.service.js
@@ -1,6 +1,6 @@
 const axios = require('axios'); 
 
 exports.createUser = async (user) => {
-    const response = await axios.post('http://localhost:4000/user',user);
+    const response = await axios.post('https://rentyback.herokuapp.com/user',user);
     return response.data;
 };
